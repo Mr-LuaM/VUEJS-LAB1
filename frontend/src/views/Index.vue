da4d6177010c9115c76116e9c2772d2ce3c2abb5
Lua <130156447+Mr-LuaM@users.noreply.github.com>
2023-10-01 22:50:30 +0800

    V0
    setup

---
+++ frontend/src/views/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <NavCom />
+  <div>ssad</div>
+</template>
+<script>
+import NavCom from "@/components/NavCom.vue";
+export default {
+  name: "Index",
+  components: {
+    NavCom,
+  },
+};
+</script>
